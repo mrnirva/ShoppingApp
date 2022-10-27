8343955c4adce4b6a6fbdd1d09c382c2edd51fb5
MrNirva <mrnirva@gmail.com>
2022-10-27 07:51:06 +0300

    Launch Screen Added

---
+++ ShoppingApp/app/src/main/java/com/shopping/app/LaunchActivity.kt
@@ -0,0 +1,11 @@
+package com.shopping.app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LaunchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_launch)
+    }
+}
