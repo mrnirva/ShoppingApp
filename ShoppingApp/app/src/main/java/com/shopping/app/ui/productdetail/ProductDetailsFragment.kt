ae5c9514c4c06c018050686a1cf0c2b9d36b4f1e
Mr Nirva <29455604+MrNirva@users.noreply.github.com>
2022-11-04 16:58:58 +0300

    Product Details page designed and set all data, bottom layout visibility settings added

--- ShoppingApp/app/src/main/java/com/shopping/app/ui/productdetail/ProductDetailsFragment.kt
+++ ShoppingApp/app/src/main/java/com/shopping/app/ui/productdetail/ProductDetailsFragment.kt
@@ -1,24 +1,32 @@
 package com.shopping.app.ui.productdetail
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.shopping.app.R
 import com.shopping.app.data.model.Product
 import com.shopping.app.databinding.FragmentProductDetailsBinding
+import com.shopping.app.ui.productdetail.viewmodel.ProductDetailViewModel
 import com.shopping.app.utils.Constants.PRODUCT_MODEL_NAME
 
 class ProductDetailsFragment : Fragment() {
 
     private lateinit var bnd: FragmentProductDetailsBinding
+    private val viewModel by viewModels<ProductDetailViewModel>()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
 
         bnd = DataBindingUtil.inflate(inflater, R.layout.fragment_product_details, container, false)
+        bnd.viewModel = viewModel
+        bnd.productDetailsFragment = this
         return bnd.root
 
     }
@@ -30,6 +38,8 @@ class ProductDetailsFragment : Fragment() {
 
     private fun getBundleArguments(){
 
+
+
         arguments?.let {
 
             val productData = it.getString(PRODUCT_MODEL_NAME)
@@ -38,11 +48,21 @@ class ProductDetailsFragment : Fragment() {
             productData?.let { safeData ->
 
                 val product = Product.fromJson(safeData)
-                Toast.makeText(requireContext(), product.title, Toast.LENGTH_LONG).show()
+                bnd.dataHolder = product
+
+                viewModel.productCountLiveData.observe(viewLifecycleOwner){ value ->
+
+                    bnd.basketCount = value
+
+                }
 
             }
         }
 
+    }
+
+    fun goBack(){
+        findNavController().popBackStack()
     }
 
 }
