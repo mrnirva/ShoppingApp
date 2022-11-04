63a5e65f285e90fe80f175217546a57e8796be4b
Mr Nirva <29455604+MrNirva@users.noreply.github.com>
2022-11-04 16:58:58 +0300

    Product Details page designed and set all data, bottom layout visibility settings added

--- ShoppingApp/app/src/main/java/com/shopping/app/ui/main/mainbase/MainMenuFragment.kt
+++ ShoppingApp/app/src/main/java/com/shopping/app/ui/main/mainbase/MainMenuFragment.kt
@@ -6,10 +6,16 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.shopping.app.R
 import com.shopping.app.databinding.FragmentMainMenuBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainMenuFragment : Fragment() {
 
@@ -27,6 +33,10 @@ class MainMenuFragment : Fragment() {
 
         val navHostFragment = childFragmentManager.findFragmentById(R.id.navHostFragmentContainer) as NavHostFragment
         NavigationUI.setupWithNavController(bnd.bottomNav, navHostFragment.navController)
+
+        navHostFragment.navController.addOnDestinationChangedListener { controller, destination, arguments ->
+            bnd.isBottomNavVisible = destination.id != R.id.productDetailsFragment
+        }
 
     }
 
